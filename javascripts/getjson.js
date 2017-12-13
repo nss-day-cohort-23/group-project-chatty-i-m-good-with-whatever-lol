216079de5bb76babffa643c7ecc44c1afd9016c3
Griffin <griffin@Griffins-MacBook-Air.local>
2017-12-13 11:05:06 -0600

    added getjson module, added function to push json message to message array

---
+++ javascripts/getjson.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let msgArr = [];
+
+module.exports.addToArr = (obj) =>{
+  console.log('this is happening?');
+  msgArr.push(obj);
+
+};
+
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,23 @@
 'use strict';
+// let messages = require('./messages');
+let getjson = require('./getjson');
 
 const messageReq = new XMLHttpRequest();
+
 const parseData = () => {
     const data = JSON.parse(event.target.responseText).messages;
     console.log(event.target);
 
+    getjson.addToArr(data);
+    
 };
+// console.log('data', parseData());
 
 messageReq.addEventListener('load', parseData);
 messageReq.open("GET","data.json");
-    console.log("This is happening");
+    // console.log("This is happening");
 messageReq.send();
+
+
+
+// module.exports
